748f0ec3b86f2f205d5aec96868aeed0340b72c8
DDemmer1 <Dennis.Demmer@web.de>
2018-04-26 13:37:27 +0200

    added Pyplot image view

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
+#Mindest groesse um als objekt erkannt zu werden
 MIN_CONTOUR_AREA = 1000
 
 RESIZED_IMAGE_WIDTH = 20
@@ -15,6 +17,7 @@ if brandberg_065 is None:
 
 imgGray = cv2.cvtColor(brandberg_065, cv2.COLOR_BGR2GRAY)
 imgBlurred = cv2.GaussianBlur(imgGray, (5, 5), 0)
+#cv2.imshow("Blurred", imgBlurred)
 
 imgThresh = cv2.adaptiveThreshold(imgBlurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
@@ -24,24 +27,34 @@ imgContours, npaContours, npaHierarchy = cv2.findContours(imgThreshCopy, cv2.RET
 
 npaFlattenedImages = np.empty((0, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
 
-i = 0
 
 for npaContour in npaContours:
 
     if cv2.contourArea(npaContour) > MIN_CONTOUR_AREA:
         [intX, intY, intW, intH] = cv2.boundingRect(npaContour)
 
-        cv2.rectangle(brandberg_065, (intX, intY), (intX + intW, intY + intH),(0, 0, 255),2)
+        cv2.rectangle(brandberg_065, (intX, intY), (intX + intW, intY + intH),(255, 0, 0),2)
 
         imgROI = imgThresh[intY:intY + intH, intX:intX + intW]
         # imgROIResized = cv2.resize(imgROI, (RESIZED_IMAGE_WIDTH,RESIZED_IMAGE_HEIGHT))
 
         imgROI = cv2.bitwise_not(imgROI)
-        cv2.imshow("imgROI", imgROI)
+        #cv2.imshow("imgROI", imgROI)
         # cv2.imshow("imgROIResized", imgROIResized)
         cv2.waitKey(0)
         cv2.destroyWindow("imgROI")
 
-cv2.imshow("pic", brandberg_065)
+
+
+# Verkleinert ein Bild um die haelfte
+#small = cv2.resize(brandberg_065, (0,0), fx=0.5, fy=0.5)
+
+
+#Besser mit pyplot darstellen wegen Zoom etc.
+#cv2.imshow("pic", brandberg_065)
+
+imgplot = plt.imshow(brandberg_065)
+
+plt.show()
 
 cv2.waitKey(0)
